3e8cc5f5348cc5bcba00d07a71f6f6b25857a8bd
rsedger <richard.sedger@gmail.com>
2016-12-07 22:42:26 +1300

    still wrangling data locations

--- R/data.R
+++
@@ -1,5 +0,0 @@
-#' US National Highway Traffic Safety Administration fatal accident data
-#'
-#' @source Uinted States NHTSA \url{http://http://www.nhtsa.gov/Data/Fatality-Analysis-Reporting-System-(FARS)}
-#' @format A bzipped csv file
-#'
